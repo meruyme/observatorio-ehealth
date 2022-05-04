4412db5156ea0d5167e7c466bd5e8e94aa9b68cb
Mellany Linhares <mellinhares6@gmail.com>
2022-05-03 22:15:29 -0300

    feat: Add view da página inicial

--- gerenciamento/urls.py
+++ gerenciamento/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'gerenciamento'
 
 urlpatterns = [
-    path('entrar/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name="auth-login", ),
-    path('sair/', auth_views.LogoutView.as_view(), name="auth-logout"),
+    path('entrar/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name="login", ),
+    path('sair/', auth_views.LogoutView.as_view(), name="logout"),
+    path('', views.home, name='home')
 ]
--- observatorioehealth/settings.py
+++ observatorioehealth/settings.py
@@ -147,5 +147,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# LOGIN_URL = 'login'
-# LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
