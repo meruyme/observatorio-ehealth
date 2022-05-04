25912d2ddbed2b90c27e63a0453792480a546969
Mellany Linhares <mellinhares6@gmail.com>
2022-05-04 00:05:09 -0300

    feat: Add models no admin

--- gerenciamento/admin.py
+++ gerenciamento/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from gerenciamento.models import Coordenador, Aluno, Pais, Hospital, User
+
+admin.site.register(Coordenador)
+admin.site.register(Aluno)
+admin.site.register(Pais)
+admin.site.register(Hospital)
+admin.site.register(User)
