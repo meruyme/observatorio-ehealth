c1c259835ad936ed4e6814bcbdc606127ea14bb4
Mellany Linhares <mellinhares6@gmail.com>
2022-05-10 00:45:38 -0300

    fix: Ajuste no local do arquivo forms

--- gerenciamento/views.py
+++ gerenciamento/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render, redirect
 
-from forms import SalvarAlunoForm, SalvarHospitalForm, SalvarCoordenadorForm, RedefinirSenhaForm
+from gerenciamento.forms import SalvarAlunoForm, SalvarHospitalForm, SalvarCoordenadorForm, RedefinirSenhaForm
 from django.contrib.auth import get_user_model
 
 from gerenciamento.choices import TipoUsuario
