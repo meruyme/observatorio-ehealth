a6f8c624002542dba7ff7a55dbc500188bff362a
Mellany Linhares <mellinhares6@gmail.com>
2022-05-14 17:29:10 -0300

    feat: Ajustes na página de edição

---
+++ gerenciamento/static/js/geral.js
@@ -0,0 +1,38 @@
+let isEditable = false;
+
+$(".select").select2({
+    theme: "bootstrap4"
+});
+
+function restore() {
+    $('form').find(':input').each(function(i, elem) {
+        let input = $(elem);
+        input.val(input.data('initialState'));
+        input.trigger("change");
+    });
+}
+
+$(document).ready(() => {
+    $("input, select").prop('disabled', true);
+
+    $('form').find(':input').each(function(i, elem) {
+        let input = $(elem);
+        input.data('initialState', input.val());
+    });
+
+    $('#btn-editar').on('click', () => {
+        if(isEditable){
+            restore();
+            $("input, select").prop('disabled', true);
+            $('#btn-editar').text('Editar');
+            $('#btn-cadastro').addClass('d-none');
+            isEditable = false;
+        }
+        else{
+            $("input, select").prop('disabled', false);
+            $('#btn-editar').text('Desfazer alterações');
+            $('#btn-cadastro').removeClass('d-none');
+            isEditable = true;
+        }
+    })
+});
