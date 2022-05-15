8ac17691b849b78a735cc416d2c5c28c81f264ff
Mellany Linhares <mellinhares6@gmail.com>
2022-05-14 22:35:52 -0300

    refactor: Mudando ordem dos fields do form de salvar pesquisa

--- pesquisa/forms.py
+++ pesquisa/forms.py
@@ -7,7 +7,7 @@ from pesquisa.models import Pesquisa
 class SalvarPesquisaForm(forms.ModelForm):
     class Meta:
         model = Pesquisa
-        fields = ['titulo', 'data_inicio', 'data_fim', 'hospitais', 'perguntas', 'alunos']
+        fields = ['titulo', 'data_inicio', 'data_fim', 'alunos', 'hospitais', 'perguntas']
         widgets = {
             'hospitais': Select2MultipleWidget,
             'perguntas': Select2MultipleWidget,
