0da68b3901a1b36834156e773caeab550fddb57e
Mellany Linhares <mellinhares6@gmail.com>
2022-05-15 20:15:31 -0300

    build: Ajustes nas staticfiles

--- observatorioehealth/settings.py
+++ observatorioehealth/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
