5cb718e5e84fcca39e8dff57b603d3e0be1526ff
Mellany Linhares <mellinhares6@gmail.com>
2022-05-15 21:03:16 -0300

    fix: Correção

--- observatorioehealth/settings.py
+++ observatorioehealth/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY', True)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
